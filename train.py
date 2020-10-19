cd6cafd767230b1a043ce98ac050dc9410eab80f
diyar-m <kordboydm@gmail.com>
2020-10-19 16:18:09 +0330

    Edit train.py to continue training

--- train.py
+++ train.py
@@ -39,8 +39,8 @@ class Trainer:
             self.adv_trainer.full_d.load_state_dict(torch.load('last_full_d_fpn.h5')['model'])
             self.scheduler_G.load_state_dict(torch.load('last_scheduler_G_fpn.h5')['model'])
             self.scheduler_D.load_state_dict(torch.load('last_scheduler_D_fpn.h5')['model'])
-            self.optimizer_G.load_state_dict(torch.load('last_optimizer_G_fpn.h5')['model'])
-            self.optimizer_D.load_state_dict(torch.load('last_optimizer_D_fpn.h5')['model'])
+            self.optimizer_G.load_state_dict(torch.load('last_optimizer_G_fpn.h5'))
+            self.optimizer_D.load_state_dict(torch.load('last_optimizer_D_fpn.h5'))
             start_epoch += 29
         for epoch in range(start_epoch, config['num_epochs']):
             if (epoch == self.warmup_epochs) and not (self.warmup_epochs == 0):
