2b660d54bf2d4592bfdbae59d925072e4606318c
diyar-m <kordboydm@gmail.com>
2020-10-19 15:58:12 +0330

    Edit train.py to continue training

--- train.py
+++ train.py
@@ -32,15 +32,16 @@ class Trainer:
 
     def train(self, resume_train=False):
         self._init_params()
-
-        self.netG.load_state_dict(torch.load('last_G_{}.h5'.format(self.config['experiment_desc'])))
-        self.adv_trainer.patch_d.load_state_dict(torch.load('last_patch_d_{}.h5'.format(self.config['experiment_desc'])))
-        self.adv_trainer.full_d.load_state_dict(torch.load('last_full_d_{}.h5'.format(self.config['experiment_desc'])))
-        self.scheduler_G.load_state_dict(torch.load('last_scheduler_G_{}.h5'.format(self.config['experiment_desc'])))
-        self.scheduler_D.load_state_dict(torch.load('last_scheduler_D_{}.h5'.format(self.config['experiment_desc'])))
-        self.optimizer_G.load_state_dict(torch.load('last_optimizer_G_{}.h5'.format(self.config['experiment_desc'])))
-        self.optimizer_D.load_state_dict(torch.load('last_optimizer_D_{}.h5'.format(self.config['experiment_desc'])))
-        start_epoch = 0 + 29
+        start_epoch = 0
+        if resume_train:
+            self.netG.load_state_dict(torch.load('last_G_{}.h5'.format(self.config['experiment_desc'])))
+            self.adv_trainer.patch_d.load_state_dict(torch.load('last_patch_d_{}.h5'.format(self.config['experiment_desc'])))
+            self.adv_trainer.full_d.load_state_dict(torch.load('last_full_d_{}.h5'.format(self.config['experiment_desc'])))
+            self.scheduler_G.load_state_dict(torch.load('last_scheduler_G_{}.h5'.format(self.config['experiment_desc'])))
+            self.scheduler_D.load_state_dict(torch.load('last_scheduler_D_{}.h5'.format(self.config['experiment_desc'])))
+            self.optimizer_G.load_state_dict(torch.load('last_optimizer_G_{}.h5'.format(self.config['experiment_desc'])))
+            self.optimizer_D.load_state_dict(torch.load('last_optimizer_D_{}.h5'.format(self.config['experiment_desc'])))
+            start_epoch += 29
         for epoch in range(start_epoch, config['num_epochs']):
             if (epoch == self.warmup_epochs) and not (self.warmup_epochs == 0):
                 self.netG.module.unfreeze()
