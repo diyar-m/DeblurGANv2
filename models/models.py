d49e5ee17b608dc2345155f34f6993958ef0a75f
diyar-m <kordboydm@gmail.com>
2020-12-09 05:21:29 +0330

    fix bug in get_images_and_metrics to support batch size > 1

--- models/models.py
+++ models/models.py
@@ -21,14 +21,21 @@ class DeblurModel(nn.Module):
         image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
         return image_numpy.astype(imtype)
 
-    def get_images_and_metrics(self, inp, output, target) -> (float, float, np.ndarray):
-        inp = self.tensor2im(inp)
-        fake = self.tensor2im(output.data)
-        real = self.tensor2im(target.data)
-        psnr = PSNR(fake, real)
-        ssim = SSIM(fake, real, multichannel=True)
-        vis_img = np.hstack((inp, fake, real))
-        return psnr, ssim, vis_img
+    def get_images_and_metrics(self, inps, outputs, targets) -> (float, float, np.ndarray):
+        psnr = 0
+        ssim = 0
+        for i in range(len(inps)):
+            inp = inps[i:i + 1]
+            output = outputs[i:i + 1]
+            target = targets[i:i + 1]
+
+            inp = self.tensor2im(inp)
+            fake = self.tensor2im(output.data)
+            real = self.tensor2im(target.data)
+            psnr += PSNR(fake, real)
+            ssim += SSIM(fake, real, multichannel=True)
+            vis_img = np.hstack((inp, fake, real))
+        return psnr / len(inps), ssim / len(inps), vis_img
 
 
 def get_model(model_config):
